006eb9b04b158cca74309d4311ff9b5d7e46f30c
LikeSys <leesh86030@gmail.com>
2026-05-13 14:42:19 +0900

    20260513

--- aws_infra/00_backend/terraform.tf
+++ aws_infra/00_backend/terraform.tf
@@ -6,14 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3" {
-    bucket = "aws08-terraform-state-bucket"
-    key = "s3/terraform.tfstate"
-    region = "ap-northeast-2"
-    dynamodb_table = "aws08-terraform-locks"
-    #use_lockfile = true
-    encrypt = true
-  }
+  # backend "s3" {
+  #   bucket = "aws08-terraform-state-bucket"
+  #   key = "s3/terraform.tfstate"
+  #   region = "ap-northeast-2"
+  #   dynamodb_table = "aws08-terraform-locks"
+  #   #use_lockfile = true
+  #   encrypt = true
+  # }
 }
 
 provider "aws" {
